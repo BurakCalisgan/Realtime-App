61c2146edf315675672a1a33fc8452a2c7fb06ea
BurakCalisgan <calisganburak@gmail.com>
2024-05-17 21:33:40 +0300

    Basic edit.

--- src/backend/realtime-socket/src/server.js
+++ src/backend/realtime-socket/src/server.js
@@ -44,6 +44,10 @@ symbolService.startSendingData();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req,res) => {
+  res.send('Realtime App Socket!');
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
